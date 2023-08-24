764ad23441559b8efb60065031d2fa17fee5103d
Filip Działa <fi.dziala@gmail.com>
2023-08-24 19:40:49 +0200

    Add project files.

---
+++ IamprogrammerVI_B/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IamprogrammerVI_B
+{
+    internal class BankAccount
+    {
+        private string owner;
+        private double money;
+
+
+
+        public BankAccount(string owner, double money)
+        {
+            this.money = money;
+            this.owner = owner;
+        }
+
+        public void printSaldo()
+        {
+            Console.WriteLine("Hi, {0} your saldo in bank account equals {1}", owner, money);
+        }
+
+        public string Owner
+
+        { 
+            get
+            { 
+                return owner; 
+            } 
+        }
+
+        public double Money
+        {
+            get
+            {
+                return money;
+            }
+        }
+
+        public static BankAccount operator +(BankAccount bank1, BankAccount bank2)
+        {
+         double newAmountOfMoney = bank1.Money + bank2.Money;
+            string newOwners = bank1.Owner + "+" + bank2.Owner;
+            BankAccount newBankAccount = new BankAccount(newOwners, newAmountOfMoney);
+
+            return newBankAccount;
+
+        }
+
+        public static BankAccount operator +(BankAccount bank1, double amount)
+        {
+            double newAmountOfMoney = bank1.Money + amount;
+            BankAccount bank3 = new BankAccount(bank1.Owner, newAmountOfMoney);
+            return bank3;
+
+        }
+    }
+}
---
+++ IamprogrammerVI_B/Program.cs
@@ -0,0 +1,37 @@
+﻿using IamprogrammerVI_B;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Intrinsics.Arm;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IamprogrammerVI
+{
+    class Program
+    {
+        // 
+        static void Main(string[] args)
+        {
+            BankAccount b1 = new BankAccount("Filip", 1000);
+            b1.printSaldo();
+            Console.WriteLine(b1.Owner);
+            Console.WriteLine(b1.Money);
+
+            BankAccount b2 = new BankAccount("Paweł", 500);
+            b2.printSaldo();
+            Console.WriteLine(b2.Owner);
+            Console.WriteLine(b2.Money);
+
+            BankAccount b3 = b1 + b2;
+            Console.WriteLine(b3.Owner);
+            Console.WriteLine(b3.Money);
+
+            BankAccount b4 = b1 + 400;
+            Console.WriteLine(b4.Owner);
+            Console.WriteLine(b4.Money);
+
+
+        }
+    }
+}
